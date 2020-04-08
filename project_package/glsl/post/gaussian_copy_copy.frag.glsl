9c9637ee1f1ad2f37ec8e5cb1652e148f2d0c5db
Nathan Devlin <ndevlin@seas.upenn.edu>
2020-04-08 15:31:14 -0400

    Procedural Dust looking pretty good

--- project_package/glsl/post/gaussian_copy_copy.frag.glsl
+++ project_package/glsl/post/gaussian_copy_copy.frag.glsl
@@ -33,6 +33,55 @@ vec3 textureDistorted(
                 );
 }
 
+// Takes in a vec2 and generates a pseudorandom but consistent value for it
+float noise2D(vec2 n)
+{
+    float toReturn = (fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453)); //* 16 + 128.f;
+
+    return 10.0 * pow(toReturn, 1000.0);
+}
+
+float interpNoise2D(float x, float y)
+{
+    int intX = int(floor(x));
+    float fractX = fract(x);
+    int intY = int(floor(y));
+    float fractY = fract(y);
+
+    float v1 = noise2D(vec2(intX, intY));
+    float v2 = noise2D(vec2(intX + 1, intY));
+    float v3 = noise2D(vec2(intX, intY + 1));
+    float v4 = noise2D(vec2(intX + 1, intY + 1));
+
+    float i1 = mix(v1, v2, fractX);
+    float i2 = mix(v3, v4, fractX);
+    return mix(i1, i2, fractY);
+}
+
+
+
+
+float fbm(float x, float y)
+{
+    float total = 0;
+    float persistence = 0.5f;
+    int octaves = 8;
+
+    for(int i = 1; i <= octaves; i++)
+    {
+        float freq = pow(2.f, i);
+        float amp = pow(persistence, i);
+
+        total += interpNoise2D(x * freq, y * freq) * amp;
+    }
+    return total;
+}
+
+
+
+
+
+
 
 void main()
 {
@@ -110,9 +159,26 @@ void main()
         inColor = vec4(1.0, 1.0, 1.0, 1.0);
     }
 
+
     vec4 scaledValue = max(vec4(0.0), inColor + bias) * scale;
+
+
+    float dustVal = 100.0 * fbm(fs_UV[0] * u_Dimensions[0], fs_UV[1] * u_Dimensions[1]);
+
+
+
+
+
 
     color += vec3(scaledValue * haloWeight);
 
+
+    color = vec3(dustVal);
+
+
+    float val = 100.0 * pow(clamp(fbm(fs_UV[0], fs_UV[1]), 0.0, 0.9), 0.9);
+
+
+    color = vec3(val);
 
 }
